e97053c5b47357eaf1e6cc1c2c85cb0f527baf9f
AgataMal <98488410+AgataMal@users.noreply.github.com>
2022-07-25 20:19:23 +0200

    main page fixes #5 (#6)

---
+++ server/page_recipes.py
@@ -0,0 +1,19 @@
+page_content="""<html>
+    <body>
+        <b><i>my recipes </i>  </b>
+        <table>
+
+            <tr>
+                <td>
+                    goulash
+                </td>
+                <td><a href="/goulash">recipe</a></td>
+            </tr>
+            <tr>
+                <td>dumplings</td>
+                <td><a href="/dumplings">recipe</a></td>         
+            </tr>
+        </table>
+    </body>
+</html>
+"""
