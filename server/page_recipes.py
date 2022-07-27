964e158137c745392fc35f5f1fa7f9d2d1a3b839
Agata <malczyk.agata88@gmail.com>
2022-07-27 20:32:02 +0200

    new layout

--- server/page_recipes.py
+++ server/page_recipes.py
@@ -1,19 +1,35 @@
-page_content="""<html>
-    <body>
-        <b><i>my recipes </i>  </b>
-        <table>
+page_content="""
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Recipes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
 
-            <tr>
-                <td>
-                    goulash
-                </td>
-                <td><a href="/goulash">recipe</a></td>
-            </tr>
-            <tr>
-                <td>dumplings</td>
-                <td><a href="/dumplings">recipe</a></td>         
-            </tr>
-        </table>
+
+    <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+        <div class="container-fluid p-5 bg-primary text-white text-center">
+            <h1>Recipes</h1>
+            <p>by Agata Malczyk</p>
+        </div>
+        
+        <div class="container mt-5">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>goulash </h3>
+                <p><a href="/goulash">recipe</a></p>
+            </div>
+            <div class="col-sm-4">
+                <h3>dumplings</h3>
+                <p><a href="/dumplings">recipe</a></p>
+            </div>
+            
+        </div>
+        </div>
+
     </body>
 </html>
 """
