6a61053f4549ca07a74ace0878ffaed7a73d38b2
Agata <malczyk.agata88@gmail.com>
2022-07-27 20:53:49 +0200

    layout 3

--- server/page_goulash_test.py
+++ server/page_goulash_test.py
@@ -4,7 +4,7 @@ import page_goulash
 class TestGoulash(unittest.TestCase):
 
     def test_dumplings(self):
-        self.assertEqual(page_goulash.recipe().index(page_goulash.name), 30)
+        self.assertEqual(page_goulash.recipe().index(page_goulash.name), 715)
 
 if __name__ == '__main__':
     unittest.main()
