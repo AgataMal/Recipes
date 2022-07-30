d9cfdecc3a44ed059073df147d1ee1df7d1fa0df
Agata <malczyk.agata88@gmail.com>
2022-07-30 13:56:57 +0200

    recipe template

--- server/page_goulash.py
+++ server/page_goulash.py
@@ -1,6 +1,8 @@
 import page_recipe_template
+from flask import render_template 
+
 name="goulash"
 picture="https://www.mojegotowanie.pl/media/cache/default_view/uploads/media/recipe/0001/100/gulasz-wieprzowy-z-szynki.jpeg"
 
 def recipe():
-    return page_recipe_template.page_template.format(name, picture)
+    return render_template(page_recipe_template.template_name, recipe_name=name, recipe_image=picture)
