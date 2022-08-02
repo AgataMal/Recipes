32ef684967fbb53b871037a3ff8f0fc11106712b
AgataMal <98488410+AgataMal@users.noreply.github.com>
2022-08-02 20:58:39 +0200

    new recipe (#24)

---
+++ server/page_devolay.py
@@ -0,0 +1,8 @@
+import page_recipe_template
+from flask import render_template 
+
+name="devolay"
+picture="https://www.mojegotowanie.pl/media/cache/default_view/uploads/media/default/0001/24/6ebf5afdab82aed3a98232569344003fb31172ac.jpeg"
+
+def recipe():
+    return render_template(page_recipe_template.template_name, recipe_name=name, recipe_image=picture, ingredients= ["meat","butter","spices"])
