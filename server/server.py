fe73884f4a25eaa340379b5fe8bdc9526df36c16
Agata <malczyk.agata88@gmail.com>
2022-08-02 20:57:45 +0200

    new recipe

--- server/server.py
+++ server/server.py
@@ -3,6 +3,8 @@ import page_recipes
 import page_goulash
 import page_dumplings
 import page_cheesecake
+import page_devolay
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -12,7 +14,9 @@ def home():
     return render_template(page_recipes.template_name,recipies=[
         {"recipe_name":"dumplings","recipe_path":"dumplings"},
         {"recipe_name":"goulash","recipe_path":"goulash"},
-        {"recipe_name":"cheesecake","recipe_path":"cheesecake"}])
+        {"recipe_name":"cheesecake","recipe_path":"cheesecake"},
+        {"recipe_name":"devolay","recipe_path":"devolay"}])
+
 
 @app.route('/goulash')
 def goulash():
@@ -26,7 +30,9 @@ def dumplings():
 def cheesecake():
     return page_cheesecake.recipe()
 
-
+@app.route('/devolay')
+def devolay():
+    return page_devolay.recipe()
 
 
 if __name__ == '__main__':
